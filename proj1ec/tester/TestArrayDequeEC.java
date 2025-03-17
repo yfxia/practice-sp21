fb7915622944e1ef4d1653e560e905bd3be4fb20
yfxia <sophia.xia2012@gmail.com>
2025-03-17 18:59:22 -0400

    Test Proj1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -2,9 +2,13 @@ package tester;
 
 import static org.junit.Assert.*;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
+
+import java.util.Deque;
+import java.util.Optional;
 
 public class TestArrayDequeEC {
     /** Randomly call StudentArrayDeque and ArrayDequeSolution methods
@@ -21,32 +25,44 @@ public class TestArrayDequeEC {
         assertEquals("Size of ArrayDeque should be 1", sad1.size(), ads1.size());
     }
 
+    public Integer addFirstRemoveStudent(int num, StudentArrayDeque<Integer> deque) {
+
+        deque.addFirst(num);
+        deque.addFirst(num*2);
+        return deque.removeFirst();
+    }
+
+    public Integer addFirstRemoveSolution(int num, ArrayDequeSolution<Integer> deque) {
+
+        deque.addFirst(num);
+        deque.addFirst(num*2);
+        return deque.removeFirst();
+    }
+
+    public Integer addLastRemoveStudent(int num, StudentArrayDeque<Integer> deque) {
+
+        deque.addLast(num);
+        deque.addLast(num+3);
+        return deque.removeLast();
+    }
+    public Integer addLastRemoveSolution(int num, ArrayDequeSolution<Integer> deque) {
+
+        deque.addLast(num);
+        deque.addLast(num+3);
+        return deque.removeLast();
+    }
+
     @Test
     public void addRemoveTest() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
-        for (int i = 0; i < 1000; i++) {
+
+        for (int i = 0; i < 100; i++) {
             double uniRandomNum = StdRandom.uniform();
             if (uniRandomNum < 0.5) {
-                sad1.addFirst(i);
-                ads1.addFirst(i);
+                assertEquals(addFirstRemoveStudent(i, sad1), addFirstRemoveSolution(i, ads1));
             } else {
-                sad1.addLast(i);
-                ads1.addLast(i);
-            }
-        }
-        for (int i = 0; i < 1000; i++) {
-            double uniRandomNum = StdRandom.uniform();
-            if (uniRandomNum < 0.5) {
-                Integer sad1First = sad1.removeFirst();
-                Integer ads1First = ads1.removeFirst();
-                assertEquals("removeFirst(), student was " + sad1First
-                        + " correct was " + ads1First, sad1First, ads1First);
-            } else {
-                Integer sad1Last = sad1.removeLast();
-                Integer ads1Last = ads1.removeLast();
-                assertEquals("RemoveLast(), student was " + sad1Last
-                        + " correct was " + ads1Last, sad1Last, ads1Last);
+                assertEquals(addLastRemoveStudent(i, sad1), addLastRemoveSolution(i, ads1));
             }
         }
 
