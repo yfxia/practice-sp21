058f55746e46714ceb2ceedc0601480cc58ec4e9
yfxia <sophia.xia2012@gmail.com>
2025-03-17 20:34:06 -0400

    Failure Sequence updated

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -25,42 +25,13 @@ public class TestArrayDequeEC {
         assertEquals("Size of ArrayDeque should be 1", sad1.size(), ads1.size());
     }
 
-    public Integer addFirstRemoveStudent(int num, StudentArrayDeque<Integer> deque) {
-
-        deque.addFirst(num);
-        deque.addFirst(num*2);
-        return deque.removeFirst();
-    }
-
-    public Integer addFirstRemoveSolution(int num, ArrayDequeSolution<Integer> deque) {
-
-        deque.addFirst(num);
-        deque.addFirst(num*2);
-        return deque.removeFirst();
-    }
-
-    public Integer addLastRemoveStudent(int num, StudentArrayDeque<Integer> deque) {
-
-        deque.addLast(num);
-        deque.addLast(num+3);
-        deque.addFirst(num+4);
-        return deque.removeLast();
-    }
-    public Integer addLastRemoveSolution(int num, ArrayDequeSolution<Integer> deque) {
-
-        deque.addLast(num);
-        deque.addLast(num+3);
-        deque.addFirst(num+4);
-        return deque.removeLast();
-    }
-
     @Test
     public void addRemoveTest() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
         StringBuilder methodCalls = new StringBuilder();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             double uniRandomNum = StdRandom.uniform();
             if (uniRandomNum < 0.5) {
                 sad1.addFirst(i);
@@ -69,6 +40,9 @@ public class TestArrayDequeEC {
                 ads1.addFirst(i);
                 ads1.addFirst(i*2);
                 methodCalls.append("addFirst(").append(i*2).append(")").append("\n");
+                sad1.addLast(i+3);
+                ads1.addLast(i+3);
+                methodCalls.append("addLast(").append(i+3).append(")").append("\n");
                 Integer sad1First = sad1.removeFirst();
                 Integer ads1First = ads1.removeFirst();
                 methodCalls.append("RemoveFirst()").append("\n");
@@ -80,10 +54,13 @@ public class TestArrayDequeEC {
                 ads1.addLast(i);
                 ads1.addLast(i+1);
                 methodCalls.append("addLast(").append(i+1).append(")").append("\n");
+                ads1.addFirst(i+4);
+                sad1.addFirst(i+4);
+                methodCalls.append("addFirst(").append(i+4).append(")").append("\n");
                 Integer sad1Last = sad1.removeLast();
                 Integer ads1Last = ads1.removeLast();
-                methodCalls.append("RemoveFirst()").append("\n");
-                assertEquals(String.valueOf(methodCalls), sad1Last, ads1Last);
+                methodCalls.append("RemoveLast()").append("\n");
+                assertEquals(String.valueOf(methodCalls), ads1Last, sad1Last);
             }
         }
 
