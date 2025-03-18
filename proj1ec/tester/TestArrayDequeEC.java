2a3262ac553726326222aeb4ac95ad42fb3005dd
yfxia <sophia.xia2012@gmail.com>
2025-03-17 20:15:17 -0400

    Build failure sequence for Proj1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -58,13 +58,32 @@ public class TestArrayDequeEC {
     public void addRemoveTest() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+        StringBuilder methodCalls = new StringBuilder();
 
         for (int i = 0; i < 1000; i++) {
             double uniRandomNum = StdRandom.uniform();
             if (uniRandomNum < 0.5) {
-                assertEquals(addFirstRemoveStudent(i, sad1), addFirstRemoveSolution(i, ads1));
+                sad1.addFirst(i);
+                sad1.addFirst(i*2);
+                methodCalls.append("addFirst(").append(i).append(")").append("\n");
+                ads1.addFirst(i);
+                ads1.addFirst(i*2);
+                methodCalls.append("addFirst(").append(i*2).append(")").append("\n");
+                Integer sad1First = sad1.removeFirst();
+                Integer ads1First = ads1.removeFirst();
+                methodCalls.append("RemoveFirst()").append("\n");
+                assertEquals(String.valueOf(methodCalls), ads1First, sad1First);
             } else {
-                assertEquals(addLastRemoveStudent(i, sad1), addLastRemoveSolution(i, ads1));
+                sad1.addLast(i);
+                sad1.addLast(i+1);
+                methodCalls.append("addLast(").append(i).append(")").append("\n");
+                ads1.addLast(i);
+                ads1.addLast(i+1);
+                methodCalls.append("addLast(").append(i+1).append(")").append("\n");
+                Integer sad1Last = sad1.removeLast();
+                Integer ads1Last = ads1.removeLast();
+                methodCalls.append("RemoveFirst()").append("\n");
+                assertEquals(String.valueOf(methodCalls), sad1Last, ads1Last);
             }
         }
 
