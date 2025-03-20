2e684b344af40256f3d59b79ec41bebb47d336e4
yfxia <sophia.xia2012@gmail.com>
2025-03-19 23:21:04 -0400

    Proj1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -30,6 +30,8 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
         StringBuilder methodCalls = new StringBuilder();
+        Integer sad1Remove;
+        Integer ads1Remove;
 
         for (int i = 0; i < 100; i++) {
             double uniRandomNum = StdRandom.uniform();
@@ -40,17 +42,10 @@ public class TestArrayDequeEC {
                 ads1.addFirst(i*2);
                 methodCalls.append("addFirst(").append(i-1).append(")").append("\n");
                 methodCalls.append("addFirst(").append(i*2).append(")").append("\n");
-//                sad1.addLast(i+3);
-//                ads1.addLast(i+3);
-//                methodCalls.append("addLast(").append(i+3).append(")").append("\n");
-//                Integer sad1First = sad1.removeFirst();
-//                Integer ads1First = ads1.removeFirst();
-//                methodCalls.append("RemoveFirst()").append("\n");
-//                assertEquals(String.valueOf(methodCalls), ads1First, sad1First);
-                Integer sad1Last = sad1.removeLast();
-                Integer ads1Last = ads1.removeLast();
-                methodCalls.append("RemoveLast()").append("\n");
-                assertEquals(String.valueOf(methodCalls), ads1Last, sad1Last);
+                sad1Remove = sad1.removeFirst();
+                ads1Remove = ads1.removeFirst();
+                methodCalls.append("RemoveFirst()").append("\n");
+                assertEquals(String.valueOf(methodCalls), ads1Remove, sad1Remove);
             } else {
                 sad1.addLast(i-10);
                 sad1.addLast(i+1);
@@ -61,10 +56,10 @@ public class TestArrayDequeEC {
                 ads1.addFirst(i+4);
                 sad1.addFirst(i+4);
                 methodCalls.append("addFirst(").append(i+4).append(")").append("\n");
-                Integer sad1Last = sad1.removeLast();
-                Integer ads1Last = ads1.removeLast();
+                sad1Remove = sad1.removeLast();
+                ads1Remove = ads1.removeLast();
                 methodCalls.append("RemoveLast()").append("\n");
-                assertEquals(String.valueOf(methodCalls), ads1Last, sad1Last);
+                assertEquals(String.valueOf(methodCalls), ads1Remove, sad1Remove);
             }
         }
 
