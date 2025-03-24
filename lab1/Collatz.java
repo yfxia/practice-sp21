c66221bd0fa0ef99274959361aaa8aa7a030cbac
yfxia <sophia.xia2012@gmail.com>
2025-03-23 20:25:25 -0400

    Lab4A: Finish! Completed Lab4A

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,10 +1,17 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @Sophia Xia
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        return n % 2 == 0 ? n/2 : 3*n + 1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
